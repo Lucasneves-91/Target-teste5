6b9cf54768bb0b3788252f79a9043b092558c2e3
Lucas Neves <lucas.ccdr@gmail.com>
2024-09-09 19:26:47 -0300

    Incluido Scanner para interação.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        String original = "Oi pessoal!";
+        Scanner leitura = new Scanner(System.in);
+        String palavra = leitura.nextLine();
+
         StringBuilder inverso = new StringBuilder();
 
-        for (int i = original.length() - 1; i >= 0; i--) {
-            inverso.append(original.charAt(i));
+        for (int i = palavra.length() - 1; i >= 0; i--) {
+            inverso.append(palavra.charAt(i));
         }
 
         System.out.println(inverso);
